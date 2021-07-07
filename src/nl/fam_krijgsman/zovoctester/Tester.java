51f3980d2a6ac990f84fd0884d96fade72fb8ecc
Martijn Krijgsman <m.s.krijgsman@gmail.com>
2021-07-07 21:24:08 +0200

    Klasse diagram uitgewerkt

--- src/nl/fam_krijgsman/zovoctester/Tester.java
+++ src/nl/fam_krijgsman/zovoctester/Tester.java
@@ -2,7 +2,7 @@ package nl.fam_krijgsman.zovoctester;
 
 import nl.fam_krijgsman.zovoctester.model.UserLoginModelTest;
 
-public class Main {
+public class Tester {
     public static void main(String[] args) {
         System.out.println("UserLoginModelTest:");
         UserLoginModelTest.test();
