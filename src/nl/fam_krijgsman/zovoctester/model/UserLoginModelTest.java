a308246fcde8bb43dea185282dc55cfbb292128e
Martijn Krijgsman <m.s.krijgsman@gmail.com>
2021-07-08 21:42:47 +0200

    junit tests toegevoegd

--- src/nl/fam_krijgsman/zovoctester/model/UserLoginModelTest.java
+++ src/nl/fam_krijgsman/zovoctester/model/UserLoginModelTest.java
@@ -1,43 +1,33 @@
 package nl.fam_krijgsman.zovoctester.model;
 
 import nl.fam_krijgsman.zovoc.model.UserLoginModel;
+import org.junit.jupiter.api.Test;
 
-public class UserLoginModelTest {
-    static UserLoginModel userLoginModel = new UserLoginModel();
+import static org.junit.jupiter.api.Assertions.*;
 
-    public static void test() {
+class UserLoginModelTest {
+
+    @Test
+    void isValidLogin() {
         // test 1 niet bestaande gebruiker
+        UserLoginModel userLoginModel = new UserLoginModel();
         String user = "test";
         String password = "test";
-        System.out.println("Expect fout:");
-        checkLogin(user, password);
-        System.out.println();
+        assertFalse(userLoginModel.isValidLogin(user, password));
 
+        // test 2 fout wachtwoord
         user = "martijn";
         password = "martijn";
-        System.out.println("Expect fout:");
-        checkLogin(user, password);
-        System.out.println();
+        assertFalse(userLoginModel.isValidLogin(user, password));
 
+        // test 3 valide inlog
         user = "martijn";
         password = "test";
-        System.out.println("Expect goed:");
-        checkLogin(user, password);
-        System.out.println();
+        assertTrue(userLoginModel.isValidLogin(user, password));
 
+        // test 4 valide inlog
         user = "zovocuser";
         password = "zovocpassword";
-        System.out.println("Expect goed:");
-        checkLogin(user, password);
-        System.out.println();
-
-    }
-
-    private static void checkLogin(String user, String password) {
-        if (userLoginModel.isValidLogin(user, password)) {
-            System.out.println(user + " : " + "goed");
-        } else {
-            System.out.println(user + " : " + "fout");
-        }
+        assertTrue(userLoginModel.isValidLogin(user, password));
     }
 }
