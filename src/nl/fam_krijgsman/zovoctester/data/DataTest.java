4a65800ffafc56dd7210a15e62a1dc48ea9551f1
Martijn Krijgsman <m.s.krijgsman@gmail.com>
2021-07-10 16:39:01 +0200

    Datatest gemaakt

---
+++ src/nl/fam_krijgsman/zovoctester/data/DataTest.java
@@ -0,0 +1,31 @@
+package nl.fam_krijgsman.zovoctester.data;
+
+import nl.fam_krijgsman.zovoc.data.LidData;
+import nl.fam_krijgsman.zovoc.data.TeamData;
+import nl.fam_krijgsman.zovoc.data.UserLoginData;
+import nl.fam_krijgsman.zovoc.model.UserLoginModel;
+import nl.fam_krijgsman.zovoc.model.classes.Vereniging;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DataTest {
+
+    @Test
+    void UserLoginDataTest() {
+        UserLoginModel userLoginModel = new UserLoginModel();
+        UserLoginData.addTestUsers(userLoginModel);
+        assertFalse(userLoginModel.isValidLogin("martijn", "martijn"));
+        assertTrue(userLoginModel.isValidLogin("martijn", "test"));
+    }
+
+    @Test
+    void TeamandLidDataTest() {
+        Vereniging vereniging = new Vereniging("test");
+        TeamData.addTeamData(vereniging);
+        LidData.addLidData(vereniging);
+        assertEquals(9, vereniging.getTeams().size());
+        assertEquals(45, vereniging.getLeden().size());
+    }
+
+}
