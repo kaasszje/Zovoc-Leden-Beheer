3a0d728197c0a57aa93cc09e334281dfa0716da5
Martijn Krijgsman <m.s.krijgsman@gmail.com>
2021-07-10 22:37:23 +0200

    switchPanel gemaakt

--- src/nl/fam_krijgsman/zovoc/controller/BeheerController.java
+++ src/nl/fam_krijgsman/zovoc/controller/BeheerController.java
@@ -3,12 +3,40 @@ package nl.fam_krijgsman.zovoc.controller;
 import nl.fam_krijgsman.zovoc.model.BeheerModel;
 import nl.fam_krijgsman.zovoc.view.BeheerView;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class BeheerController {
-    BeheerView beheerView;
-    BeheerModel beheerModel;
+    private BeheerView beheerView;
+    private BeheerModel beheerModel;
 
     public BeheerController(BeheerView beheerView, BeheerModel beheerModel) {
         this.beheerView = beheerView;
         this.beheerModel = beheerModel;
+
+        this.beheerView.ledenMenuListener(new LedenMenuListener());
+        this.beheerView.teamsMenuListener(new TeamMenuListener());
+        this.beheerView.exitMenuListener(new ExitMenuListener());
+    }
+
+    class LedenMenuListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            beheerView.switchPanel(beheerView.getLedenPanel());
+        }
+    }
+
+    class TeamMenuListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            beheerView.switchPanel(beheerView.getTeamPanel());
+        }
+    }
+
+    class ExitMenuListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
     }
 }
